d198d59869f58a1851e26ff9517001e1d652dadf
Home Windows Thinkpad <petyr.notion@gmail.com>
2025-09-29 22:23:25 +0800

    Initial MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SpendSmart_NET8_Minimalist.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
